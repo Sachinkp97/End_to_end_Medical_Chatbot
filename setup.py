fd5fe650a00995588c9861e4f2c00d0f08e911a7
Sachin K P <sachinkp97@gmail.com.com>
2025-03-24 00:30:52 +0530

    "initial_commit"

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="GEN_AI_MED_PROJECT",
+    version="0.1.0",
+    author="Sachin K P",
+    author_email="sachinkp97@gmail.com",
+    description="A medical research project using GEN-AI",
+    packages=find_packages(),
+    install_requires=[]
+)
